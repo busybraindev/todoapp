fe00d73dc7201ba0b40e27c31a92a273d8a7b1ce
Mubarak Adeyemi <busybraindev@gmail.com>
2026-06-23 22:43:22 +0100

    Fixed

--- src/components/Td.jsx
+++ src/components/Td.jsx
@@ -23,21 +23,21 @@ const Td = () => {
     inputRef.current.value = "";
   };
   return (
-    <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
+    <div className="bg-white place-self-center w-11/12 max-w-80 flex flex-col p-7 min-h-[550px] rounded-xl">
       <div className="flex items-center mt-7 gap-2">
         <img src={todo}></img>
         <h1 className="text-3xl font-semibold">To-Do List</h1>
       </div>
-      <div className="flex items-center my-7 bg-gray-200 rounded-full">
+      <div className="flex items-center  my-7 bg-rose-200 rounded-full w-[290px]">
         <input
           ref={inputRef}
-          className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
+          className="bg-transparent border-0 outline-none h-14 pl-6 pr-2 placeholder:text-slate-600"
           type="text"
           placeholder="Add task"
         />
         <button
           onClick={add}
-          className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+          className="border-none rounded-full bg-orange-600 w-32 h-12 text-white text-base font-medium cursor-pointer"
         >
           ADD +
         </button>
